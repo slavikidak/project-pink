2467995bd4dabff2f04dd827ff9173fcf53b724d
slavikidak <slavikidak@gmail.com>
2022-01-28 18:04:24 +0000

    Deploying to gh-pages from @ slavikidak/project-pink@9a68337dcf86825aeeba5c9996d0cc20a9477b03 🚀

--- src.f947f961.js
+++ src.f947f961.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/about/mobile/milk-spray-mobile-left.png":[["milk-spray-mobile-left.953f2b5a.png","FeWR"],"FeWR"],"./../images/about/mobile/milk-spray-mobile-right.png":[["milk-spray-mobile-right.1b37d86a.png","DnBf"],"DnBf"],"./../images/about/mobile/milk-spray-mobile-left@2x.png":[["milk-spray-mobile-left@2x.6d8c8550.png","hAVS"],"hAVS"],"./../images/about/mobile/milk-spray-mobile-right@2x.png":[["milk-spray-mobile-right@2x.f5be9e07.png","H0GI"],"H0GI"],"/home/runner/work/project-pink/project-pink/src/images/contacts/desktop/bg@1x.png":[["bg@1x.c99f0828.png","xbqx"],"xbqx"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project-pink/src.c15eb46c.js.map
+//# sourceMappingURL=/project-pink/src.f947f961.js.map
